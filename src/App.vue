1b19e7c943553a9d9d062d045c42107911620096
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-06-11 14:37:15 +0300

    add form to SmsImobis component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container" id="app">
+    <SmsImobis />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SmsImobis from './components/SmsImobis.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    SmsImobis
   }
 }
 </script>
---
+++ src/components/SmsImobis.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="sms">
+    <form action="">
+      <div class="form-row">
+        <textarea class="form-control" rows="5" v-model="smsText" @input="getSmsTextLength"></textarea>
+      </div>
+      <div class="form-row mt-3">
+        <div class="form-group col-md-4 justify-content-start">
+          <label class="form-check-label">
+            <input type="checkbox" class="form-check-input" value="">Транслитерировать
+          </label>
+        </div>
+        <div class="form-group col-md-8 text-right">
+          <span>Символов: {{ smsTextLength }}</span>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SmsImobis',
+  data() {
+    return {
+      smsText: '',
+      smsTextLength: 0
+    }
+  },
+
+  methods: {
+    getSmsTextLength() {
+      this.smsTextLength = this.smsText.length;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
