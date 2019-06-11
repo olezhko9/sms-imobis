9779ae6c4f13e612774a10204865c7705a472517
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-06-12 00:20:32 +0300

    sms counting

--- src/components/SmsImobis.vue
+++ src/components/SmsImobis.vue
@@ -2,17 +2,17 @@
   <div class="sms">
     <form action="">
       <div class="form-row">
-        <textarea class="form-control" rows="5" v-model="smsText" @input="getSmsTextLength"></textarea>
+        <textarea class="form-control" rows="5" v-model="smsText" @input="updateSmsInfo"></textarea>
       </div>
       <div class="form-row mt-3">
-        <div class="form-group col-md-4 justify-content-start">
+        <div class="form-group col-md-4">
           <label class="switch">
             <input type="checkbox" v-model="showTranslit" @click="transliterateSmsText">
             <span class="slider round"></span>
           </label>Транслитерировать
         </div>
         <div class="form-group col-md-8 text-right">
-          <span>Символов: {{ smsTextLength }}</span>
+          <span>Символов: {{ smsTextLength }} ({{ smsCount }})</span>
         </div>
       </div>
     </form>
@@ -28,22 +28,53 @@ export default {
       smsOriginalText: '',
       smsTransliteratedText: '',
       smsTextLength: 0,
+      smsCount: 0,
       showTranslit: false
     }
   },
 
   methods: {
+    updateSmsInfo() {
+      this.getSmsTextLength();
+      this.getSmsCount();
+    },
+
     getSmsTextLength() {
       this.smsTextLength = this.smsText.length;
     },
 
+    getSmsCount() {
+      let segmentLength, shortenedSegmentLength;
+      if (this.containsOnlyGSM()) {
+        segmentLength = 160;
+        shortenedSegmentLength = 153;
+      } else {
+        segmentLength = 70;
+        shortenedSegmentLength = 67;
+      }
+
+      if (this.smsTextLength === 0) this.smsCount = 0;
+      else if (this.smsTextLength <= segmentLength) this.smsCount = 1;
+      else this.smsCount = Math.ceil(this.smsTextLength / shortenedSegmentLength);
+    },
+
+    containsOnlyGSM() {
+      // GSM 7-bit alphabet from https://messente.com/blog/most-recent/how-is-sms-length-calculated
+      const GSMCharacters = "@£$¥èéùìòÇØø\nÅåΔ_ΦΓΛΩΠΨΣΘΞ'ÆæßÉ !\"#¤%&()*=,-./0123456789:;<=>?¡ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÑÜ¿abcdefghijklmnopqrstuvwxyzäöñüà";
+      const GSMExtendedCharacters = "|^€{}[]~\\";
+      for (let char of this.smsText) {
+        if (GSMCharacters.indexOf(char) < 0 && GSMExtendedCharacters.indexOf(char) < 0) return false;
+      }
+      return true;
+    },
+
     transliterateSmsText() {
-      let translit = {
+      const translit = {
         'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i',
         'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
         'ф': 'f', 'х': 'h', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'sch', 'ъ': '\'', 'ы': 'i', 'ь': '\'', 'э': 'e',
         'ю': 'u', 'я': 'ya', '«': '"', '»': '"', '–': '-', '—': '-', '№': '#', '`': '\''
-      }
+      };
 
       function capitalize(str) {
         return str[0].toUpperCase() + str.slice(1);
@@ -68,7 +99,7 @@ export default {
         this.smsTransliteratedText = '';
       }
 
-      this.getSmsTextLength();
+      this.updateSmsInfo();
     }
   }
 }
